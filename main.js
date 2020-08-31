49e2d5eebb34ad557ea63983d40a449d1543b926
Ron Hamenahem <brklyn8900@gmail.com>
2020-08-31 17:54:47 -0400

    initial commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
+    width: 1200,
     height: 600,
     webPreferences: {
       nodeIntegration: true
