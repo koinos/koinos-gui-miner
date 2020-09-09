839d69728f251da03b5a7f566d0d73a6431722f0
Steve Gerbino <steve@gerbino.co>
2020-09-09 01:58:24 -0400

    #5: Clean up visual feedback, power button now reacts to on/off events

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const { ipcRenderer } = require('electron');
 
 const endpoint = 'http://localhost:8545';
 
-function toggleColor() {
+function powerButton(state) {
   let btn = document.getElementById("svg-button").classList;
   let btnOn = document.getElementById("power-button").classList;
-  if (btn.contains('red')) {
+  if (state) {
     btn.remove('red');
     btn.add('green');
     btnOn.remove('redBorder');
@@ -16,15 +16,13 @@ function toggleColor() {
     btnOn.remove('greenBorder');
     btnOn.add('redBorder');
   }
-
 }
 
-
 function toggleMiner() {
-  toggleColor();
-  var ethAddress = document.getElementById("ethAddress").value;
-  var enableTip = document.getElementById("tip").checked;
-  var tip = 0;
+  let ethAddress = document.getElementById("ethAddress").value;
+  let enableTip = document.getElementById("tip").checked;
+  let tip = 0;
+
   if (enableTip) {
     tip = 5;
   }
@@ -33,8 +31,9 @@ function toggleMiner() {
 }
 
 ipcRenderer.on('koin-balance-update', (event, arg) => {
-  let result = (arg / 1000000).toFixed(6).toString();
-  if (arg > 1000000) {
+  const decimalPlaces = 6;
+  let result = (arg / (10 ** decimalPlaces)).toString();
+  if (arg > (10 ** decimalPlaces)) {
     let parts = result.split(".");
     result = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "." + parts[1];
   }
@@ -53,16 +52,24 @@ ipcRenderer.on('hashrate-report', (event, arg) => {
   console.log(data);
 });
 
-ipcRenderer.on('miner-activated', (event, arg) => {
-  console.log("miner activated:" + arg);
+ipcRenderer.on('miner-activated', (event, state) => {
+  console.log("miner activated:" + state);
 
-  if (arg) {
+  if (state) {
     document.getElementById("tip").setAttribute("disabled", "true");
     document.getElementById("check-toggle").className += " grayed";
+
+    document.getElementById("hashrate-current").innerHTML = "...";
+    document.getElementById("hashrate-suffix").innerHTML = "";
   }
   else {
     document.getElementById("tip").removeAttribute("disabled");
     document.getElementById("check-toggle").classList.remove("grayed");
+
+    document.getElementById("hashrate-current").innerHTML = "0.0";
+    document.getElementById("hashrate-suffix").innerHTML = "";
   }
+
+  powerButton(state);
 });
 
