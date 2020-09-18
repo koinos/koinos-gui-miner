5ba52a210993b655e1f4c2fda838037d9db84b5b
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-17 22:37:30 -0400

    Nuke the custom name

--- ci/after_success_linux.sh
+++ ci/after_success_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-electron-packager . KoinosGUIMiner --out dist/
+electron-packager . --out dist/
 electron-installer-debian --src dist/KoinosGUIMiner-linux-x64/ --dest dist/installer/ --arch amd64
 ls dist/installer
