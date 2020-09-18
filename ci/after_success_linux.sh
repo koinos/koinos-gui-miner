9dec9e18bacdf800ae27f125a03598a6d6190fd1
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-17 22:30:04 -0400

    Fix linux output app location

--- ci/after_success_linux.sh
+++ ci/after_success_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 electron-packager . KoinosGUIMiner --out dist/
-electron-installer-debian --src dist/KoinosGUIMiner/ --dest dist/installer/ --arch amd64
+electron-installer-debian --src dist/KoinosGUIMiner-linux-x64/ --dest dist/installer/ --arch amd64
 ls dist/installer
