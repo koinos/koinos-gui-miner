aa8f876d8754452f6119811c5503d5dbca8e32af
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-17 22:48:22 -0400

    Fix name

--- ci/after_success_linux.sh
+++ ci/after_success_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 electron-packager . --out dist/
-electron-installer-debian --src dist/KoinosGUIMiner-linux-x64/ --dest dist/installer/ --arch amd64
+electron-installer-debian --src dist/koinos-gui-miner-linux-x64/ --dest dist/installer/ --arch amd64
 ls dist/installer
