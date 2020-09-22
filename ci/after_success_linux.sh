56db2b35c4d42c6c0d3583c2a501b04a9c3729b8
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-22 14:40:17 -0400

    Update linux

--- ci/after_success_linux.sh
+++ ci/after_success_linux.sh
@@ -2,4 +2,4 @@
 
 electron-packager . --out dist/
 cp KnsToken.json "dist/Koinos Miner-linux-x64/"
-electron-installer-debian --src "dist/Koinos Miner-linux-x64/" --dest dist/installer/ --arch amd64
+electron-installer-debian --src "dist/Koinos Miner-linux-x64/" --dest dist/installer/ --arch amd64 --options.bin "Koinos Miner"
