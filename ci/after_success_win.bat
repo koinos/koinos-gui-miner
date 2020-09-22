75882c34c7dd12db2b6a9787c8bc2fa10b25604a
nathan <nathan@nbcaldwell.com>
2020-09-21 21:59:18 -0400

    Attempt to fix build

--- ci/after_success_win.bat
+++ ci/after_success_win.bat
@@ -1,3 +1,3 @@
-call electron-packager . --out dist/
-call cp KnsToken.json "dist/Koinos Miner-win32-x64/"
-call electron-installer-windows --src "dist/Koinos Miner-win32-x64/" --dest dist/installer/ --options.exe "Koinos Miner.exe"
+call electron-packager . koinos-gui-miner --out dist/
+call cp KnsToken.json "dist/koinos-gui-miner-win32-x64/"
+call electron-installer-windows --src "dist/koinos-gui-miner-win32-x64/" --dest dist/installer/ --options.exe "koinos-gui-miner.exe"
