17972872f244eb2417331804f423ae2a2001aea5
nathan <nathan@nbcaldwell.com>
2020-09-21 22:08:01 -0400

    stuff

--- ci/after_success_win.bat
+++ ci/after_success_win.bat
@@ -1,3 +1,3 @@
-call electron-packager . koinos-gui-miner --out dist/
+call electron-packager . --out dist/
 call cp KnsToken.json "dist/koinos-gui-miner-win32-x64/"
-call electron-installer-windows --src "dist/koinos-gui-miner-win32-x64/" --dest dist/installer/ --options.exe "koinos-gui-miner.exe"
+call electron-installer-windows --src dist/koinos-gui-miner-win32-x64/ --dest dist/installer/
