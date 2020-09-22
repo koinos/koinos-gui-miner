45ca38aace6d2fb4e38a29018337e73545ae99ae
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-22 14:00:33 -0400

    Fix names

--- ci/after_success_linux.sh
+++ ci/after_success_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 electron-packager . --out dist/
-cp KnsToken.json dist/koinos-gui-miner-linux-x64/
-electron-installer-debian --src dist/koinos-gui-miner-linux-x64/ --dest dist/installer/ --arch amd64
+cp KnsToken.json "dist/Koinos Miner-linux-x64/"
+electron-installer-debian --src "dist/Koinos Miner-linux-x64/" --dest dist/installer/ --arch amd64
