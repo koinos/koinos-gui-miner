4b0db3ce6b2b8a4ffa054c87888e78a235c6cf48
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-23 16:25:43 -0400

    Add missing call

--- ci/after_success_win.bat
+++ ci/after_success_win.bat
@@ -1,3 +1,3 @@
 call electron-packager . --out dist/
 call cp KnsToken.json "dist/Koinos Miner-win32-x64/"
-iscc /DMyAppVersion=$(python ci/get_version.py package.json) win32.iss
+call iscc /DMyAppVersion=$(python ci/get_version.py package.json) win32.iss
