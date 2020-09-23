8ba008a5d8d0bf9110e0171ae627485cab6a7063
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-23 16:09:59 -0400

    Fixes

--- ci/after_success_osx.sh
+++ ci/after_success_osx.sh
@@ -3,4 +3,4 @@
 electron-packager . --out dist/
 cp KnsToken.json "dist/Koinos Miner-darwin-x64/"
 electron-installer-dmg "dist/Koinos Miner-darwin-x64/" "Koinos Miner" --out dist/installer/ --title "Koinos GUI Miner"
-mv dist/installer/$(ls dist/installer/) dist/installer/KoinosMiner$(python ci/get_version.py package.json).dmg
+mv "dist/installer/$(ls dist/installer/)" "dist/installer/KoinosMiner$(python ci/get_version.py package.json).dmg"
