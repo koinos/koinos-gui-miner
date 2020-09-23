642be9a4175852d35ddbe3f940263b42ea4e72e8
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-23 14:47:56 -0400

    Fix package names Update to only release on tagged

--- ci/after_success_linux.sh
+++ ci/after_success_linux.sh
@@ -3,3 +3,4 @@
 electron-packager . --out dist/
 cp KnsToken.json "dist/Koinos Miner-linux-x64/"
 electron-installer-debian --src "dist/Koinos Miner-linux-x64/" --dest dist/installer/ --arch amd64 --options.bin "Koinos Miner"
+mv dist/installer/$(ls dist/installer/) dist/installer/KoinosMiner$(python ci/get_version.py package.json).deb
