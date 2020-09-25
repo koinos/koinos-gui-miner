076b6a9b4d128a91c69086afe70b1db194597d3a
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-25 12:26:44 -0400

    Package fixes

--- ci/after_success_osx.sh
+++ ci/after_success_osx.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 electron-packager . --out dist/
-cp KnsToken.json "dist/Koinos Miner-darwin-x64/"
-electron-installer-dmg "dist/Koinos Miner-darwin-x64/" "Koinos Miner" --out dist/installer/ --title "Koinos GUI Miner"
+electron-installer-dmg "dist/Koinos Miner-darwin-x64/Koinos Miner.app" "Koinos Miner" --out dist/installer/ --title "Koinos GUI Miner"
 mv "dist/installer/$(ls dist/installer/)" "dist/installer/KoinosMiner$(python ci/get_version.py package.json).dmg"
--- ci/after_success_win.bat
+++ ci/after_success_win.bat
@@ -1,5 +1,4 @@
 call electron-packager . --out dist/
-call cp KnsToken.json "dist/Koinos Miner-win32-x64/"
 
 FOR /F "tokens=*" %%V in ('python ci/get_version.py package.json') DO ( SET VERSION=%%V )
 call iscc /DMyAppVersion=%VERSION% win32.iss
