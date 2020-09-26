bab352c44b744ef5dd14b66c411983b7c8a8a910
Steve Gerbino <steve@gerbino.co>
2020-09-26 01:33:56 -0400

    Added icon support

--- ci/after_success_win.bat
+++ ci/after_success_win.bat
@@ -1,4 +1,4 @@
-call electron-packager . --out dist/
+call electron-packager . --out dist/ --icon="./assets/icons/win/256x256.png"
 
 FOR /F "tokens=*" %%V in ('python ci/get_version.py package.json') DO ( SET VERSION=%%V )
 call iscc /DMyAppVersion=%VERSION% win32.iss
